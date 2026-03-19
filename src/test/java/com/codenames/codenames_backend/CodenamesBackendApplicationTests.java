7b8f547b67c1d27dcb462326e55a5e58b600b183
Anna Pschernig <annpschernig@edu.aau.at>
2026-03-19 16:50:47 +0100

    setup spring boot server

---
+++ src/test/java/com/codenames/codenames_backend/CodenamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenames.codenames_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenamesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
