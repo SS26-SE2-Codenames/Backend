1032b1e3cf2a27daf47d6a1572da340fb2993365
XtophB <xtophb@gmail.com>
2026-03-31 23:12:53 +0200

    feat: add method to check if spymaster's clue matches word on board

--- src/main/java/com/codenames/codenames_backend/gameplay/ClueValidationService.java
+++ src/main/java/com/codenames/codenames_backend/gameplay/ClueValidationService.java
@@ -1,4 +1,29 @@
 package com.codenames.codenames_backend.gameplay;
 
+import com.codenames.codenames_backend.playingfield.Board;
+import com.codenames.codenames_backend.playingfield.Card;
+import java.util.List;
+
 /** Handles the validation of clues from the spymaster. */
-public class ClueValidationService {}
+public class ClueValidationService {
+
+  /**
+   * Checks if the word passed by spymaster is a word already on the board.
+   *
+   * @param board the board of the game session
+   * @param word the clue from the spymaster
+   * @return the validation if the clue is valid or not
+   */
+  public boolean validateWord(Board board, String word) {
+    if (word.contains(" ")) {
+      return false;
+    }
+    List<Card> cardList = board.getCardList();
+    for (Card card : cardList) {
+      if (card.getWord().toLowerCase().equals(word.toLowerCase())) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
