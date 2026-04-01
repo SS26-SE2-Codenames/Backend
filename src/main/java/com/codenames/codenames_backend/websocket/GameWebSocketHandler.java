37b60e12dbdc602a96c2c6c018e1013ac12f0e40
tasaje1 <natasajeftic6@gmail.com>
2026-04-01 22:23:32 +0200

    feat(websocket): handle join message and register player in lobby

--- src/main/java/com/codenames/codenames_backend/websocket/GameWebSocketHandler.java
+++ src/main/java/com/codenames/codenames_backend/websocket/GameWebSocketHandler.java
@@ -1,8 +1,34 @@
 package com.codenames.codenames_backend.websocket;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 public class GameWebSocketHandler extends TextWebSocketHandler {
+
+    private final LobbyService lobbyService;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public GameWebSocketHandler(LobbyService lobbyService) {
+        this.lobbyService = lobbyService;
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        JsonNode json = mapper.readTree(message.getPayload());
+        String type = json.get("type").asText();
+
+        if(type.equals("JOIN")) {
+            String name = json.get("name").asText();
+            String code = json.get("code").asText();
+
+            Player player = new Player(name, session);
+            lobbyService.addPlayer(code,player);
+
+        }
+    }
 }
