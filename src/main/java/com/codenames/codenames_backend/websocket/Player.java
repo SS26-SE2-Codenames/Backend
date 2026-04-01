6a3e60955ee9cf3dd8ab91ad7459f1e44d72a4fd
tasaje1 <natasajeftic6@gmail.com>
2026-04-01 18:13:19 +0200

    feat(model): add player class with websocket session

---
+++ src/main/java/com/codenames/codenames_backend/websocket/Player.java
@@ -0,0 +1,22 @@
+package com.codenames.codenames_backend.websocket;
+
+import org.springframework.web.socket.WebSocketSession;
+
+public class Player {
+    private String username;
+    private WebSocketSession session;
+
+
+    public Player(String username, WebSocketSession session) {
+        this.username = username;
+        this.session = session;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public WebSocketSession getSession() {
+        return session;
+    }
+}
