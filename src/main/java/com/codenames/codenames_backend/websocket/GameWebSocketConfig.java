85134c41a4ad33b213cb3659d1c42f6928a41df5
tasaje1 <natasajeftic6@gmail.com>
2026-04-01 17:41:11 +0200

    feat(websocket): add websocket configuration and endpoint

---
+++ src/main/java/com/codenames/codenames_backend/websocket/GameWebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.codenames.codenames_backend.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+
+public class GameWebSocketConfig implements WebSocketConfigurer {
+    
+    private final GameWebSocketHandler gameWebSocketHandler;
+
+    public GameWebSocketConfig(GameWebSocketHandler gameWebSocketHandler) {
+        this.gameWebSocketHandler = gameWebSocketHandler;
+    }
+    
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(gameWebSocketHandler, "/ws/game")
+                .setAllowedOrigins("*");
+        
+        
+    }
+}
---
+++ src/main/java/com/codenames/codenames_backend/websocket/GameWebSocketHandler.java
@@ -0,0 +1,8 @@
+package com.codenames.codenames_backend.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class GameWebSocketHandler extends TextWebSocketHandler {
+}
