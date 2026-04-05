a2ec9f0c412e615d7fbaa6ece02a284e92217047
XtophB <xtophb@gmail.com>
2026-04-05 12:43:52 +0200

    feature: add clue class

---
+++ src/main/java/com/codenames/codenames_backend/gameplay/Clue.java
@@ -0,0 +1,31 @@
+package com.codenames.codenames_backend.gameplay;
+
+public class Clue {
+  private String word;
+  private int guessAmount;
+
+  /**
+   * Constructs a new clue with the word and number assigned.
+   *
+   * @param word the word used for clue
+   * @param guessAmount the amount of guesses + 1
+   */
+  public Clue(String word, int guessAmount) {
+    if (word == null || word.isEmpty()) {
+      throw new IllegalArgumentException("Word cannot be null or empty.");
+    }
+    if (guessAmount < 0) {
+      throw new IllegalArgumentException("Number cannot be negative.");
+    }
+    this.word = word;
+    this.guessAmount = guessAmount + 1;
+  }
+
+  public String getWord() {
+    return word;
+  }
+
+  public int getGuessAmount() {
+    return guessAmount;
+  }
+}
