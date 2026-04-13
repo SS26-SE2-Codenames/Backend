62d5679f7dd740b036ac4aaa0adb05c792325773
tasaje1 <natasajeftic6@gmail.com>
2026-04-13 14:36:24 +0200

    refactor: simplify Player model by removing WebSocketSession

--- src/main/java/com/codenames/codenames_backend/websocket/Player.java
+++ src/main/java/com/codenames/codenames_backend/websocket/Player.java
@@ -1,11 +1,9 @@
 package com.codenames.codenames_backend.websocket;
 
-import org.springframework.web.socket.WebSocketSession;
 
 /** Represents a player connected via WebSocket. */
 public class Player {
   private final String username;
-  private WebSocketSession session;
 
   /**
    * Creates a new player.
@@ -13,9 +11,8 @@ public class Player {
    * @param username the player's username
    * @param session the WebSocket session of the player
    */
-  public Player(String username, WebSocketSession session) {
+  public Player(String username) {
     this.username = username;
-    this.session = session;
   }
 
   /**
@@ -25,23 +22,5 @@ public class Player {
    */
   public String getUsername() {
     return username;
-  }
-
-  /**
-   * Returns the WebSocket session of the player.
-   *
-   * @return the session
-   */
-  public WebSocketSession getSession() {
-    return session;
-  }
-
-  /**
-   * Sets the active {@link WebSocketSession} for this instance.
-   *
-   * @param session the WebSocket session to assign
-   */
-  public void setSession(WebSocketSession session) {
-    this.session = session;
   }
 }
