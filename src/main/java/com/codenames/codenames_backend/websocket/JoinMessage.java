b73870888bd5236fa768f2f7005df1d4ccebbb28
tasaje1 <natasajeftic6@gmail.com>
2026-04-15 18:56:09 +0200

    docs: add javadoc to JoinMessage

--- src/main/java/com/codenames/codenames_backend/websocket/JoinMessage.java
+++ src/main/java/com/codenames/codenames_backend/websocket/JoinMessage.java
@@ -3,6 +3,11 @@ package com.codenames.codenames_backend.websocket;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Message payload sent by clients to join a lobby.
+ *
+ * <p>Contains the player's name and the lobby code they want to join.
+ */
 @Getter
 @Setter
 public class JoinMessage {
