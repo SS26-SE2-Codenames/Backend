64eb561dfc93dd9f5e20bb67e9cb88b95d79e678
tasaje1 <natasajeftic6@gmail.com>
2026-04-15 19:00:26 +0200

    docs: add javadoc to Player

--- src/main/java/com/codenames/codenames_backend/websocket/Player.java
+++ src/main/java/com/codenames/codenames_backend/websocket/Player.java
@@ -2,7 +2,11 @@ package com.codenames.codenames_backend.websocket;
 
 import lombok.Getter;
 
-/** Represents a player connected via WebSocket. */
+/**
+ * Represents a player connected via WebSocket.
+ *
+ * <p>A player is identified by a username and is associated with a WebSocket session.
+ */
 @Getter
 public class Player {
   private final String username;
