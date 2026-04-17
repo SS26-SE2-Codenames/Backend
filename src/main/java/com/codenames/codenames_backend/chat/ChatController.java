7bb41f44e0fc38938b6504a2bfe6c5b249cb9c3a
XtophB <xtophb@gmail.com>
2026-04-17 17:11:17 +0200

    feat: add method to send message to own team

--- src/main/java/com/codenames/codenames_backend/chat/ChatController.java
+++ src/main/java/com/codenames/codenames_backend/chat/ChatController.java
@@ -4,13 +4,15 @@ import com.codenames.codenames_backend.lobby.services.LobbyService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-/** Intercepts client messages to server and rebroadcasts to everyone in same room. */
+/**
+ * Intercepts client messages to server and rebroadcasts to everyone in same room.
+ */
 @Controller
 public class ChatController {
+
   private final LobbyService lobbyService;
   private final SimpMessagingTemplate messagingTemplate;
 
@@ -20,7 +22,13 @@ public class ChatController {
   }
 
   @MessageMapping("/chat/{lobbyId}")
-  public void sendMessage(@DestinationVariable String lobbyId, @Payload ChatDto chatDto){
+  public void sendGlobalMessage(@DestinationVariable String lobbyId, @Payload ChatDto chatDto) {
     messagingTemplate.convertAndSend("/topic/chat/" + lobbyId, chatDto);
+  }
+
+  @MessageMapping("/chat/{lobbyId}/{team}")
+  public void sendTeamMessage(@DestinationVariable String lobbyId, @DestinationVariable String team,
+      @Payload ChatDto chatDto) {
+    messagingTemplate.convertAndSend("/topic/chat/" + lobbyId + "/" + team, chatDto);
   }
 }
