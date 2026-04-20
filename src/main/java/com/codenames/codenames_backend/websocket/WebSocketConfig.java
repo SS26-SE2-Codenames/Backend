3f2f56caa2427a1369325dc1bb38ecf28a9fa3d4
tasaje1 <natasajeftic6@gmail.com>
2026-04-20 13:41:17 +0200

    fix: allow websocket connections from android emulator and localhost

--- src/main/java/com/codenames/codenames_backend/websocket/WebSocketConfig.java
+++ src/main/java/com/codenames/codenames_backend/websocket/WebSocketConfig.java
@@ -38,6 +38,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
    */
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:8080").withSockJS();
+    registry
+        .addEndpoint("/ws")
+        .setAllowedOrigins("http://localhost:8080", "http://10.0.2.2:8080")
+        .withSockJS();
   }
 }
