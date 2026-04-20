c8351b0b2b1132ce1e4bfe60cbe4341a2437cda9
XtophB <xtophb@gmail.com>
2026-04-20 18:47:03 +0200

    refactor: controller no longer validates, move to service

--- src/main/java/com/codenames/codenames_backend/chat/ChatController.java
+++ src/main/java/com/codenames/codenames_backend/chat/ChatController.java
@@ -1,10 +1,8 @@
 package com.codenames.codenames_backend.chat;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -13,44 +11,35 @@ import org.springframework.stereotype.Controller;
  * <p>The destination is based on the lobbyID or team parameters passed when the method is invoked.
  * The parameters are appended to the destination and broadcasted to all subscribers.
  */
-@Slf4j
+
 @Controller
 public class ChatController {
 
-  private final SimpMessagingTemplate messagingTemplate;
   private final ChatService chatService;
 
   /**
    * Constructor for the ChatController.
    *
-   * @param messagingTemplate the messaging template used for broadcasting updates
    * @param chatService the service used to validate and persist chat history
    */
-  public ChatController(SimpMessagingTemplate messagingTemplate, ChatService chatService) {
-    this.messagingTemplate = messagingTemplate;
+  public ChatController(ChatService chatService) {
     this.chatService = chatService;
   }
 
   /**
    * Sends a message to all players in the same lobby.
-   *
-   * @param lobbyId the ID of the client who calls the method
+   * @param lobbyId the ID of where the message is broadcasted to
    * @param chatDto the message of the client in chatDto format
    */
   @MessageMapping("/chat/{lobbyId}")
   public void sendLobbyMessage(@DestinationVariable String lobbyId, @Payload ChatDto chatDto) {
-    try {
-      ChatDto validatedMessage = chatService.processLobbyMessage(lobbyId, chatDto);
-      messagingTemplate.convertAndSend("/topic/chat/" + lobbyId, validatedMessage);
-    } catch (IllegalArgumentException e) {
-      log.error("Invalid lobby message: {}", e.getMessage());
-    }
+    chatService.processLobbyMessage(lobbyId, chatDto);
   }
 
   /**
    * Sends a message to all players in the same lobby and on the same team.
    *
-   * @param lobbyId the ID of the client who calls the method
+   * @param lobbyId the ID of where the message is broadcasted to
    * @param team the team of the client who calls the method
    * @param chatDto the message of the client in chatDto format
    */
@@ -59,11 +48,6 @@ public class ChatController {
       @DestinationVariable String lobbyId,
       @DestinationVariable String team,
       @Payload ChatDto chatDto) {
-    try {
-      ChatDto validatedMessage = chatService.processTeamMessage(lobbyId, team, chatDto);
-      messagingTemplate.convertAndSend("/topic/chat/" + lobbyId + "/" + team, validatedMessage);
-    } catch (IllegalArgumentException e) {
-      log.error("Invalid team message: {}", e.getMessage());
-    }
+    chatService.processTeamMessage(lobbyId, team, chatDto);
   }
 }
--- src/main/java/com/codenames/codenames_backend/chat/ChatService.java
+++ src/main/java/com/codenames/codenames_backend/chat/ChatService.java
@@ -2,13 +2,21 @@ package com.codenames.codenames_backend.chat;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 /** Service responsible for processing, validating and storing messages. */
+@Slf4j
 @Service
 public class ChatService {
+  private final SimpMessagingTemplate messagingTemplate;
   // Each lobby now stores 3 COWAL
   private final Map<String, ChatHistory> lobbyChatHistory = new ConcurrentHashMap<>();
+
+  public ChatService(SimpMessagingTemplate messagingTemplate) {
+    this.messagingTemplate = messagingTemplate;
+  }
 
   private static void validateDto(ChatDto chatDto) {
     if (chatDto.senderUsername() == null || chatDto.senderUsername().isEmpty()) {
@@ -27,16 +35,19 @@ public class ChatService {
    *
    * @param lobbyId the ID of the lobby
    * @param chatDto the message to process and store
-   * @return the validated chat message
+   * @throws IllegalArgumentException if validation fails
    */
-  public ChatDto processLobbyMessage(String lobbyId, ChatDto chatDto) {
-    validateDto(chatDto);
+  public void processLobbyMessage(String lobbyId, ChatDto chatDto) {
+    try {
+      validateDto(chatDto);
 
-    ChatHistory currentLobbyChat =
-        lobbyChatHistory.computeIfAbsent(lobbyId, k -> new ChatHistory());
-    currentLobbyChat.addLobbyMessage(chatDto);
-
-    return chatDto;
+      ChatHistory currentLobbyChat =
+          lobbyChatHistory.computeIfAbsent(lobbyId, k -> new ChatHistory());
+      currentLobbyChat.addLobbyMessage(chatDto);
+      messagingTemplate.convertAndSend("/topic/chat/" + lobbyId, chatDto);
+    } catch (IllegalArgumentException e) {
+      log.error("Invalid lobby message: {}", e.getMessage());
+    }
   }
 
   /**
@@ -45,15 +56,19 @@ public class ChatService {
    * @param lobbyId the ID of the lobby
    * @param team the color of the team ("RED" or "BLUE")
    * @param chatDto the message to process and store
-   * @return the validated chat message
+   * @throws IllegalArgumentException if validation fails
    */
-  public ChatDto processTeamMessage(String lobbyId, String team, ChatDto chatDto) {
-    validateDto(chatDto);
+  public void processTeamMessage(String lobbyId, String team, ChatDto chatDto) {
+    try {
+      validateDto(chatDto);
 
-    ChatHistory currentTeamChat = lobbyChatHistory.computeIfAbsent(lobbyId, k -> new ChatHistory());
-    currentTeamChat.addTeamMessage(team, chatDto);
-
-    return chatDto;
+      ChatHistory currentTeamChat =
+          lobbyChatHistory.computeIfAbsent(lobbyId, k -> new ChatHistory());
+      currentTeamChat.addTeamMessage(team, chatDto);
+      messagingTemplate.convertAndSend("/topic/chat/" + lobbyId + "/" + team, chatDto);
+    } catch (IllegalArgumentException e) {
+      log.error("Invalid team message: {}", e.getMessage());
+    }
   }
 
   /**
