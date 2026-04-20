ae68a82c03d258ebc9a015ff232ae7bf6e615fd7
XtophB <xtophb@gmail.com>
2026-04-20 03:20:13 +0200

    test: fix sonar issue, now streaming parameterized tests

--- src/test/java/com/codenames/codenames_backend/chat/ChatServiceTest.java
+++ src/test/java/com/codenames/codenames_backend/chat/ChatServiceTest.java
@@ -2,10 +2,15 @@ package com.codenames.codenames_backend.chat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import com.codenames.codenames_backend.chat.ChatDto.MessageType;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /** Unit tests for ChatService. */
 class ChatServiceTest {
@@ -13,54 +18,27 @@ class ChatServiceTest {
   ChatDto message;
   private ChatService chatService;
 
+  private static Stream<Arguments> provideInvalidChatDto() {
+    return Stream.of(
+        arguments(new ChatDto(null, "TestMessage", MessageType.CHAT)),
+        arguments(new ChatDto("", "TestMessage", MessageType.CHAT)),
+        arguments(new ChatDto("TestName", null, MessageType.CHAT)),
+        arguments(new ChatDto("TestName", "", MessageType.CHAT)),
+        arguments(new ChatDto("TestName", "TestMessage", null)));
+  }
+
   @BeforeEach
   void setUp() {
     chatService = new ChatService();
     message = new ChatDto("TestName", "TestMessage", MessageType.CHAT);
   }
 
-  @Test
-  void testNullUsername() {
+  @ParameterizedTest(name = "[{index}] Rejects {0}")
+  @MethodSource("provideInvalidChatDto")
+  void testValidationLogic_invalidMessages(ChatDto invalidMessage) {
     assertThrows(
         IllegalArgumentException.class,
-        () ->
-            chatService.processLobbyMessage(
-                lobbyId, new ChatDto(null, "TestMessage", MessageType.CHAT)));
-  }
-
-  @Test
-  void testEmptyUsername() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            chatService.processLobbyMessage(
-                lobbyId, new ChatDto("", "TestMessage", MessageType.CHAT)));
-  }
-
-  @Test
-  void testNullContent() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            chatService.processLobbyMessage(
-                lobbyId, new ChatDto("TestName", null, MessageType.CHAT)));
-  }
-
-  @Test
-  void testEmptyContent() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            chatService.processLobbyMessage(
-                lobbyId, new ChatDto("TestName", "", MessageType.CHAT)));
-  }
-
-  @Test
-  void testNullType() {
-    assertThrows(
-        IllegalArgumentException.class,
-        () ->
-            chatService.processLobbyMessage(lobbyId, new ChatDto("TestName", "TestMessage", null)));
+        () -> chatService.processLobbyMessage(lobbyId, invalidMessage));
   }
 
   @Test
