e7f2105f59e0161952270cc22a5c251910338316
XtophB <xtophb@gmail.com>
2026-05-02 23:53:36 +0200

    refactor: remove autowire import
    SonarQube flagged two constructors and required @Autowire.
    Since @Autowire required parameters, solution was not suitable.
    Fix was to have 1 constructor and have the parameter be optional but have a default for deployment.
    This also allows flexible unit testing, which was the original reason for two constructors.

--- src/main/java/com/codenames/codenames/backend/playingfield/CardGenerator.java
+++ src/main/java/com/codenames/codenames/backend/playingfield/CardGenerator.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
