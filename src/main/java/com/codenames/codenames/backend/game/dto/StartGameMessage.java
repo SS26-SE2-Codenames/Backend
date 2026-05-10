d82409a411071f683a1657b384ca12c5c0fa3116
tasaje1 <natasajeftic6@gmail.com>
2026-05-10 18:32:01 +0200

    feat: add start game websocket dto

---
+++ src/main/java/com/codenames/codenames/backend/game/dto/StartGameMessage.java
@@ -0,0 +1,10 @@
+package com.codenames.codenames.backend.game.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StartGameMessage {
+    private String lobbyCode;
+}
