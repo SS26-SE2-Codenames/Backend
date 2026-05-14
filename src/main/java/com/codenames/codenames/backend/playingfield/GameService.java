1a9efb0804439ab0697fdd6d1511ca5b5659e8ac
XtophB <xtophb@gmail.com>
2026-05-14 17:18:06 +0200

    fix: add callingTeam parameter that should be passed by frontend
    We want frontend/ caller to pass their team to ensure that they are allowed to invoke the method or not

--- src/main/java/com/codenames/codenames/backend/playingfield/GameService.java
+++ src/main/java/com/codenames/codenames/backend/playingfield/GameService.java
@@ -1,23 +1,22 @@
 package com.codenames.codenames.backend.playingfield;
 
 import com.codenames.codenames.backend.clue.Clue;
+import com.codenames.codenames.backend.utility.Team;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GameService {
 
-  public void submitClue(GameManager gm, Clue clue) {
-    gm.submitClue(clue);
+  public void submitClue(GameManager gm, Clue clue, Team callingTeam) {
+    gm.submitClue(clue, callingTeam);
     gm.advanceTurn();
   }
 
-  public void flipCard(GameManager gm, int position) {
-    gm.flipCard(position);
-    if (gm.getRemainingGuesses() == 0) {
-      gm.advanceTurn();
-    }
+  public void flipCard(GameManager gm, int position, Team callingTeam) {
+    gm.flipCard(position, callingTeam);
   }
 
-  public void endTurn(GameManager gm) {
-    gm.advanceTurn();
+  public void passTurn(GameManager gm, Team callingTeam){
+    gm.passTurn(callingTeam);
   }
-
 }
