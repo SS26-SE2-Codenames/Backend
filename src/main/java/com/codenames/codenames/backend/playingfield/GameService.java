23ff4f59ec8fb0ff0206451ec980c839e4c0994f
XtophB <xtophb@gmail.com>
2026-05-14 17:54:19 +0200

    feat: add creating and storing GameMmanager for lobbies
    Turn system is complete but had no way of being used. This commit includes the storing and creating of GameManagers. This way we can reference a certain GameManager of a lobby to call the methods needed to advance the turns

--- src/main/java/com/codenames/codenames/backend/playingfield/GameService.java
+++ src/main/java/com/codenames/codenames/backend/playingfield/GameService.java
@@ -1,13 +1,39 @@
 package com.codenames.codenames.backend.playingfield;
 
 import com.codenames.codenames.backend.clue.Clue;
+import com.codenames.codenames.backend.clue.ClueValidationService;
 import com.codenames.codenames.backend.utility.Team;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
 
   public void submitClue(GameManager gm, Clue clue, Team callingTeam) {
+  private final CardGenerator cardGenerator;
+  private final ClueValidationService clueValidationService;
+
+  public GameService(CardGenerator cardGenerator, ClueValidationService clueValidationService) {
+    this.cardGenerator = cardGenerator;
+    this.clueValidationService = clueValidationService;
+  }
+
+  public void createGameManager(String lobbyCode, Team startingTeam) {
+    games.computeIfAbsent(
+        lobbyCode, game -> new GameManager(startingTeam, cardGenerator, clueValidationService));
+  }
+
+  public void removeGame(String lobbyCode) {
+    games.remove(lobbyCode);
+  }
+
+  public GameManager getGame(String lobbyCode) {
+    if (games.get(lobbyCode) == null) {
+      throw new IllegalStateException("GameManager does not exist for lobby: " + lobbyCode);
+    }
+    return games.get(lobbyCode);
+  }
     gm.submitClue(clue, callingTeam);
     gm.advanceTurn();
   }
