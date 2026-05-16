6c21e7f01af6a904cdd4108dec7d3b465ed2a0c6
XtophB <xtophb@gmail.com>
2026-05-16 21:35:04 +0200

    fix: update test to take current phase parameter

--- src/test/java/com/codenames/codenames/backend/serialization/DataTransferObjectServiceTest.java
+++ src/test/java/com/codenames/codenames/backend/serialization/DataTransferObjectServiceTest.java
@@ -3,6 +3,7 @@ package com.codenames.codenames.backend.serialization;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import com.codenames.codenames.backend.playingfield.Card;
@@ -21,6 +22,8 @@ class DataTransferObjectServiceTest {
   DataTransferObjectService service;
   GameStateDataTransferObject gameStateDto;
   private static final Team redTeam = Team.RED;
+  private static final Role spymaster = Role.SPYMASTER;
+  private static final Role operative = Role.OPERATIVE;
 
   @BeforeEach
   void setUp() {
@@ -36,13 +39,13 @@ class DataTransferObjectServiceTest {
     when(mockGameManager.getCurrentBlueFound()).thenReturn(0);
 
     gameStateDto =
-        service.createGameStateDataTransferObject(mockGameManager, Role.OPERATIVE, redTeam);
+        service.createGameStateDataTransferObject(mockGameManager, operative, redTeam, spymaster);
   }
 
   @Test
   void testSpymasterVisibility() {
     gameStateDto =
-        service.createGameStateDataTransferObject(mockGameManager, Role.SPYMASTER, redTeam);
+        service.createGameStateDataTransferObject(mockGameManager, spymaster, redTeam, spymaster);
     assertEquals("RED", gameStateDto.cardList().get(0).color());
   }
 
@@ -65,7 +68,7 @@ class DataTransferObjectServiceTest {
   void testGetWinner_null() {
     when(mockGameManager.getWinner()).thenReturn(null);
     gameStateDto =
-        service.createGameStateDataTransferObject(mockGameManager, Role.OPERATIVE, redTeam);
+        service.createGameStateDataTransferObject(mockGameManager, operative, redTeam, spymaster);
     assertNull(gameStateDto.winner());
   }
 }
