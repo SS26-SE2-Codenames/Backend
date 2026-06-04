11aef64ee2dd6d846be2136f120948612ebd9190
XtophB <xtophb@gmail.com>
2026-06-04 15:08:25 +0200

    feat: add lobby entity class

---
+++ src/main/java/com/codenames/codenames/backend/database/Lobby.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "lobby")
+public class Lobby {
+  @Id
+  @Column(name = "lobby_code", length = 5, nullable = false)
+  private String lobbyCode;
+  @Column(name = "created_at", insertable = false, updatable = false, nullable = false)
+  private java.sql.Timestamp createdAt;
+}
