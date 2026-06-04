995d94a8bcef31bdfb0d2ac223863e2367d75178
XtophB <xtophb@gmail.com>
2026-06-04 15:33:17 +0200

    refactor: change IDE generated POJO column to proper import

--- src/main/java/com/codenames/codenames/backend/database/Lobby.java
+++ src/main/java/com/codenames/codenames/backend/database/Lobby.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,5 +22,5 @@ public class Lobby {
   @Column(name = "lobby_code", length = 5, nullable = false)
   private String lobbyCode;
   @Column(name = "created_at", insertable = false, updatable = false, nullable = false)
-  private java.sql.Timestamp createdAt;
+  private Timestamp createdAt;
 }
