e7b446b8e5dd34ef12dbd26317e1f5ec9687bec1
XtophB <xtophb@gmail.com>
2026-06-04 17:14:52 +0200

    refactor: remove unused imports

--- src/main/java/com/codenames/codenames/backend/database/Player.java
+++ src/main/java/com/codenames/codenames/backend/database/Player.java
@@ -2,15 +2,11 @@ package com.codenames.codenames.backend.database;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
