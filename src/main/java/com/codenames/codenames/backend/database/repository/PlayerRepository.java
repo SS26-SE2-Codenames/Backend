a1dd4019e664ab54a75f494b0a857b35f70740af
XtophB <xtophb@gmail.com>
2026-06-05 23:38:49 +0200

    fix: change parameter to Long as player table uses BIGINT as the PK

--- src/main/java/com/codenames/codenames/backend/database/repository/PlayerRepository.java
+++ src/main/java/com/codenames/codenames/backend/database/repository/PlayerRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
  * Repository class for Lobby that has the default available methods needed to perform SQL queries.
  */
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, String> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 
   /**
    * Custom SQL query that is not included in the JpaRepository interface.
