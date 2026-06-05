159c5577248543f8efe40d85ba1cf7ff69d40fde
XtophB <xtophb@gmail.com>
2026-06-06 00:40:08 +0200

    fix: update imports after merge and incorrect parameters

--- src/main/java/com/codenames/codenames/backend/database/persistence/PersistenceMapper.java
+++ src/main/java/com/codenames/codenames/backend/database/persistence/PersistenceMapper.java
@@ -1,10 +1,11 @@
 package com.codenames.codenames.backend.database.persistence;
 
-import com.codenames.codenames.backend.database.Card;
+import com.codenames.codenames.backend.database.entity.CardEntity;
+import com.codenames.codenames.backend.database.entity.GameStateEntity;
+import com.codenames.codenames.backend.database.entity.LobbyEntity;
+import com.codenames.codenames.backend.database.entity.PlayerEntity;
 import com.codenames.codenames.backend.game.domain.Card;
-import com.codenames.codenames.backend.database.GameState;
-import com.codenames.codenames.backend.database.Lobby;
-import com.codenames.codenames.backend.database.Player;
+
 import com.codenames.codenames.backend.game.domain.GameManager;
 import com.codenames.codenames.backend.lobby.api.dto.PlayerDto;
 import java.util.List;
@@ -12,20 +13,18 @@ import java.util.List;
 public class PersistenceMapper {
   // Should be possible to just call save on Lobby object due to cascade. Will find out in the
   // future when this feature/ ticket is finished.
-  public Lobby mapLobbyEntity(String lobbyCode, GameManager gameManager, List<PlayerDto> players) {
-    Lobby lobby = new Lobby();
+  public LobbyEntity mapLobbyEntity(String lobbyCode, GameManager gameManager, List<PlayerDto> players) {
+    LobbyEntity lobby = new LobbyEntity();
     lobby.setLobbyCode(lobbyCode);
 
     return lobby;
   }
-  private GameState mapGameState(GameState gameState) {
-    return gameState;
+  private GameStateEntity mapGameState(GameManager gameManager) {
   }
 
-  private Card mapCard(List<Card> cards) {
-    return card;
+  private CardEntity mapCard(List<Card> cards) {
   }
-  private Player mapPlayer(List<PlayerDto> players) {
+  private PlayerEntity mapPlayer(List<PlayerDto> players) {
 
   }
 }
