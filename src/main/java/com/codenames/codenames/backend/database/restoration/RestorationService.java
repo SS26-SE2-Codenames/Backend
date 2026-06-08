81ab52f7636a4c105f715f7da91a9b68390860a2
XtophB <xtophb@gmail.com>
2026-06-09 00:53:17 +0200

    feat: add service class to handle existing restoration methods

---
+++ src/main/java/com/codenames/codenames/backend/database/restoration/RestorationService.java
@@ -0,0 +1,45 @@
+package com.codenames.codenames.backend.database.restoration;
+
+import com.codenames.codenames.backend.database.entity.LobbyEntity;
+import com.codenames.codenames.backend.database.repository.LobbyRepository;
+import com.codenames.codenames.backend.game.api.dto.GameStateDto;
+import com.codenames.codenames.backend.game.application.GameService;
+import com.codenames.codenames.backend.game.domain.GameManager;
+import com.codenames.codenames.backend.game.domain.GameManagerFactory;
+import com.codenames.codenames.backend.lobby.application.LobbyService;
+import com.codenames.codenames.backend.lobby.domain.Lobby;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestorationService {
+  private final RestorationMapper restorationMapper;
+  private final LobbyRepository lobbyRepository;
+
+  private final LobbyService lobbyService;
+  private final GameService gameService;
+  private final GameManagerFactory gameManagerFactory;
+
+  public RestorationService(RestorationMapper restorationMapper, LobbyRepository lobbyRepository,
+      LobbyService lobbyService, GameService gameService, GameManagerFactory gameManagerFactory) {
+    this.restorationMapper = restorationMapper;
+    this.lobbyRepository = lobbyRepository;
+    this.lobbyService = lobbyService;
+    this.gameService = gameService;
+    this.gameManagerFactory = gameManagerFactory;
+  }
+
+  @PostConstruct
+  public void restoreOnContainerStart() {
+    for (LobbyEntity lobbyEntity : lobbyRepository.findAll()) {
+      String lobbyCode = lobbyEntity.getLobbyCode();
+
+      Lobby lobbyObj = restorationMapper.mapToLobby(lobbyEntity);
+      lobbyService.restoreLobby(lobbyCode, lobbyObj);
+
+      GameStateDto gameStateDtoObj = restorationMapper.mapToGameStateDto(lobbyEntity);
+      GameManager gameManagerObj = gameManagerFactory.createFromSnapshot(gameStateDtoObj);
+      gameService.restoreGameManager(lobbyCode, gameManagerObj);
+    }
+  }
+}
